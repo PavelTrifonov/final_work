b70443828ccfe1cfd5156799c51c775ae3873cf3
Pavel <pashok_163@mail.ru>
2022-11-25 12:44:02 +0300

    Добавили файл с кодом решения задачи, внесли изменения в файл README

---
+++ CODE_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// ["hello", "2", "world", ":-)"]-> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] ->[]
+string[] NewArr(  string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+            count++;
+    }
+    string[] newArray = new string[count];
+    int k = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+        {
+            newArray[k] = arr[i];
+            k++;
+        }
+    }
+    return newArray;
+}
+string[] arr={"hello", "2", "world", ":-)"}; // задаем первоначальный массив
+System.Console.WriteLine(String.Join(",",NewArr(arr)));// выводим новый массив в консоль
